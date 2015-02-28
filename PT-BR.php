138b4be79a9971603dc7f312c72ecbe535c271f7
cheesypoof <cheesypoof@users.noreply.github.com>
2015-02-27 17:17:57 -0800

    Add Brazilian Portuguese (PT-BR)

---
+++ PT-BR.php
@@ -0,0 +1,246 @@
+<?php
+
+    if ( !defined('K_COUCH_DIR') ) die(); // cannot be loaded directly
+
+    // Brazilian Portuguese translation courtesy RafaelNC (http://www.couchcms.com/forum/memberlist.php?mode=viewprofile&u=17928)
+
+    ///////////EDIT BELOW THIS////////////////////////////////////////
+
+    // Header
+    $t['greeting'] = 'Ol&aacute;';
+    $t['view_site'] = 'Visualizar Site';
+    $t['logout'] = 'Sair';
+    $t['javascript_msg'] = 'O JavaScript est&aacute; desabilitado ou n&atilde;o est&aacute; sendo suportado pelo seu navegador.<br/>
+                            Por favor aprimore seu navegador ou <a title="Habilite o JavaScript no seu navegador" href="http://www.google.com/support/bin/answer.py?answer=23852"><b>enable JavaScript</b></a> para usar o Painel Administrador.';
+    $t['add_new'] = 'Adicione Novo';
+    $t['add_new_page'] = 'Adicione uma nova p&aacute;gina';
+    $t['add_new_user'] = 'Adicione um novo usu&aacute;rio';
+    $t['view'] = 'Visualizar';
+    $t['list'] = 'Listar';
+    $t['edit'] = 'Editar';
+    $t['delete'] = 'Deletar';
+    $t['delete_selected'] = 'Deletar Selecionado';
+    $t['advanced_settings'] = 'Configura&ccedil;&otilde;es Avan&ccedil;adas';
+
+    // Sidebar
+    $t['comment'] = 'Comentar';
+    $t['comments'] = 'Comment&aacute;rios';
+    $t['manage_comments'] = 'Gerenciar Coment&aacute;rios';
+    $t['users'] = 'Usu&aacute;rios';
+    $t['manage_users'] = 'Gerenciar Usu&aacute;rios';
+
+    // List pages
+    $t['view_all_folders'] = 'Ver todas as pastas';
+    $t['filter'] = 'Filtrar';
+    $t['showing'] = 'Mostrando';
+    $t['title'] = 'T&iacute;tulo';
+    $t['folder'] = 'Pasta';
+    $t['date'] = 'Data';
+    $t['actions'] = 'A&ccedil;&otilde;es';
+    $t['no_pages_found'] = 'Nenhuma p&aacute;gina encontrada';
+    $t['published'] = 'Publicado';
+    $t['unpublished'] = 'N&atilde;o publicado';
+    $t['confirm_delete_page'] = 'Voc&ecirc; tem certeza de que quer deletar esta p&aacute;gina'; // No question mark please
+    $t['confirm_delete_selected_pages'] = 'Voc&ecirc; tem certeza de que quer deletar as p&aacute;ginas selecionadas?';
+    $t['remove_template'] = 'Remover modelo';
+    $t['template_missing'] = 'Modelo desaparecido';
+    $t['prev'] = 'Anter'; // Pagination button
+    $t['next'] = 'Próx'; // Pagination button
+
+    // Pages
+    $t['welcome'] = 'Bem-vindo';
+    $t['no_regions_defined'] = 'Sem regi&otilde;es edit&aacute;veis definidas';
+    $t['no_templates_defined'] = 'Nenhum modelo est&aacute; sendo gerenciado pelo CMS';
+    $t['access_level'] = 'N&iacute;vel de Acesso';
+    $t['superadmin'] = 'Super-Administrador';
+    $t['admin'] = 'Administrador';
+    $t['authenticated_user_special'] = 'Usu&aacute;rio Autenticado (Especial)';
+    $t['authenitcated_user'] = 'Usu&aacute;rio Autenticado';
+    $t['unauthenticated_user'] = 'Todos';
+    $t['allow_comments'] = 'Permitir coment&aacute;rios de usu&aacute;rios';
+    $t['status'] = 'Estado';
+    $t['name'] = 'Nome';
+    $t['title_desc'] = 'Deixe esse campo vazio para utilizar o sistema gerador de nomes a partir do t&iacute;tulo';
+    $t['required'] = 'Requerido'; // Required field
+    $t['required_msg'] = 'Campo requerido n&atilde;o pode ser deixado vazio';
+    $t['browse_server'] = 'Procurar no Servidor';
+    $t['view_image'] = 'Visualizar Imagem';
+    $t['thumb_created_auto'] = 'Ser&aacute; criado automaticamente';
+    $t['recreate'] = 'Recriar';
+    $t['thumb_recreated'] = 'Miniaturas recriadas';
+    $t['crop_from'] = 'Recortando de';
+    $t['top_left'] = 'Topo &agrave; esquerda';
+    $t['top_center'] = 'Topo ao meio';
+    $t['top_right'] = 'Topo &agrave; direita';
+    $t['middle_left'] = 'Centro &agrave; esquerda';
+    $t['middle'] = 'Centro';
+    $t['middle_right'] = 'Centro &agrave; direita';
+    $t['bottom_left'] = 'Fundo &agrave; esquerda';
+    $t['bottom_center'] = 'Fundo ao meio';
+    $t['bottom_right'] = 'Fundo &agrave; direita';
+    $t['view_thumbnail'] = 'Visualizar miniatura';
+    $t['field_not_found'] = 'Campo n&atilde;o encontrado!';
+    $t['delete_permanently'] = 'Deletar permanentemente?';
+    $t['view_code'] = 'Visualizar C&oacute;digo';
+    $t['confirm_delete_field'] = 'Voc&ecirc; tem certeza de que quer deletar permanentemente este campo?';
+    $t['save'] = 'Salvar';
+
+    // Comments
+    $t['all'] = 'Todos';
+    $t['unapprove'] = 'Sem aprova&ccedil;&atilde;o';
+    $t['unapproved'] = 'N&atilde;o aprovados';
+    $t['approve'] = 'Aprovar';
+    $t['approved'] = 'Aprovados';
+    $t['select-deselect'] = 'Selecionar/Deselecionar todos';
+    $t['confirm_delete_comment'] = 'Voc&ecirc; tem certeza de que quer deletar esse coment&aacute;rio?';
+    $t['confirm_delete_selected_comments'] = 'Voc&ecirc; tem certeza de que quer deletar os coment&aacute;rios selecionados?';
+    $t['bulk_action'] = 'Empilhar A&ccedil;&atilde;o com os selecionados';
+    $t['apply'] = 'Aplicar';
+    $t['submitted_on'] = 'Submetido em';
+    $t['email'] = 'E-Mail';
+    $t['website'] = 'Site';
+    $t['duplicate_content'] = 'Duplicar conte&uacute;do';
+    $t['insufficient_interval'] = 'Sem espa&ccedil;o suficiente entre os coment&aacute;rios';
+
+    // Users
+    $t['user_name_restrictions'] = 'Somente letras min&uacute;sculas, n&uacute;meros, h&iacute;fen e sublinhar permitidos';
+    $t['display_name'] = 'Exibir Nome';
+    $t['role'] = 'Fun&ccedil;&atilde;o';
+    $t['no_users_found'] = 'Nenhum usu&aacute;rio encontrado';
+    $t['confirm_delete_user'] = 'Voc&ecirc; tem certeza de que quer deletar o usu&aacute;rio'; // No question mark please
+    $t['confirm_delete_selected_users'] = 'Voc&ecirc; tem certeza de que quer deletar os usu&aacute;rios selecionados?';
+    $t['disabled'] = 'Inv&aacute;lido';
+    $t['new_password'] = 'Nova Senha';
+    $t['new_password_msg'] = 'Caso queira mudar de senha, digite uma nova. Caso contr&aacute;rio, deixe isso em branco.';
+    $t['repeat_password'] = 'Repita a Senha';
+    $t['repeat_password_msg'] = 'Digite sua nova senha novamente.';
+    $t['user_name_exists'] = 'Nome de usu&aacute;rio j&aacute; existente';
+    $t['email_exists'] = 'E-Mail j&aacute; existente';
+
+    // Login
+    $t['user_name'] = 'Nome de usu&aacute;rio';
+    $t['password'] = 'Senha';
+    $t['login'] = 'Login';
+    $t['forgot_password'] = 'Esqueceu sua senha?';
+    $t['prompt_cookies'] = 'Os Cookies devem estar desabilitados a usar este CMS';
+    $t['prompt_username'] = 'Por favor digite seu nome de usu&aacute;rio';
+    $t['prompt_password'] = 'Por favor digite sua senha';
+    $t['invalid_credentials'] = 'Nome de usu&aacute;rio ou senha inv&aacute;lidos';
+    $t['account_disabled'] = 'Conta Inv&aacute;lida';
+    $t['access_denied'] = 'Acesso Negado';
+    $t['insufficient_privileges'] = 'Voc&ecirc; n&atilde;o possui privil&eacute;gios suficientes para visualizar a p&aacute;gina requesitada.
+                                    Para visualizar esta p&aacute;gina voc&ecirc; deve sair e logar com privil&eacute;gios suficientes.';
+
+    // Password recovery
+    $t['recovery_prompt'] = 'Por favor insira seu nome de usu&aacute;rio ou endere&ccedil;o de email<br/>
+                            Voc&ecirc; receber&aacute; sua senha por e-mail';
+    $t['name_or_email'] = 'Seu Nome de Usu&aacute;rio ou E-mail';
+    $t['submit'] = 'Entrar';
+    $t['submit_error'] = 'Por favor insira seu nome de usu&aacute;rio ou endere&ccedil;o de email';
+    $t['no_such_user'] = 'Tal usu&aacute;rio n&atilde;o existe.';
+    $t['reset_req_email_subject'] = 'Restabelecimento de senha requerido';
+    $t['reset_req_email_msg_0'] = 'Um pedido foi recebido para restabelecer sua senha e nome de usu&aacute;rio para o seguinte site';
+    $t['reset_req_email_msg_1'] = 'Para confirmar que o restabelecimento foi realizado por voc&ecirc;, por favor visite o seguinte endere&ccedil;o, caso contr&aacute;rio apenas ignore esse email';
+    $t['email_failed'] = 'E-Mail n&atilde;o pode ser enviado.';
+    $t['reset_req_email_confirm'] = 'Um email de confirma&ccedil;&atilde;o foi enviado para voc&ecirc;<br/>
+                                    Por favor cheque seu email.';
+    $t['invalid_key'] = 'Tecla Inv&aacute;lida';
+    $t['reset_email_subject'] = 'Sua Nova Senha';
+    $t['reset_email_msg_0'] = 'Sua senha foi restabelecida para o seguinte local e nome de usu&aacute;rio';
+    $t['reset_email_msg_1'] = 'Uma vez que logar, a senha poder&aacute; ser trocada.';
+    $t['reset_email_confirm'] = 'Sua senha foi restabelecida<br/>
+                                Por favor cheque seu email para uma nova senha.';
+
+    // Maintenance Mode
+    $t['back_soon'] = '<h2>Modo de Manuten&ccedil;&atilde;o</h2>
+                        <p>
+                            Desculpe-nos pela inconveni&ecirc;ncia.<br/>
+                            Nosso site est&aacute; passando por manuten&ccedil;&atilde;o programada.<br/>
+                            <b>Por favor tente novamente ap&oacute;s algum tempo.</b>
+                        </p>';
+
+
+    // Addendum to Version 1.1 /////////////////////////////////////
+    // Admin Panel
+    $t['admin_panel'] = 'Painel Administrativo';
+    $t['login_title'] = 'CouchCMS';
+
+    // Folders
+    $t['no_folders'] = 'Sem pastas definidas';
+    $t['select_folder'] = 'Selecionar pasta';
+    $t['folders'] = 'Pastas';
+    $t['manage_folders'] = 'Gerenciar Pastas';
+    $t['add_new_folder'] = 'Adicionar uma nova pasta';
+    $t['parent_folder'] = 'Pasta Principal';
+    $t['weight'] = 'Import&acirc;ncia';
+    $t['weight_desc'] = 'Quanto maior o valor, menos a pasta aparecer&aacute; na lista. Pode ser colocado negativo';
+    $t['desc'] = 'Descri&ccedil;&atilde;o';
+    $t['image'] = 'Imagem';
+    $t['cannot_be_own_parent'] = 'N&atilde;o pode ser seu pr&oacute;prio principal';
+    $t['name_already_exists'] = 'Nome j&aacute; existente';
+    $t['pages'] = 'P&aacute;ginas';
+    $t['none'] = 'Nenhum';
+    $t['confirm_delete_folder'] = 'Voc&ecirc; tem certeza de que quer deletar estas pastas'; // No question mark please
+    $t['confirm_delete_selected_folders'] = 'Voc&ecirc; tem certeza de que quer deletar as pastas selecionadas?';
+
+    // Drafts
+    $t['draft_caps'] = 'RASCUNHO'; // Upper case
+    $t['draft'] = 'Rascunho';
+    $t['drafts'] = 'Rascunhos';
+    $t['create_draft'] = 'Criar Rascunho';
+    $t['create_draft_msg'] = 'Criar c&oacute;pia desta p&aacute;gina(depois de salvar altera&ccedil;&otilde;es)';
+    $t['manage_drafts'] = 'Gerenciar Rascunhos'; // Plural
+    $t['update_original'] = 'Atualizar Original';
+    $t['update_original_msg'] = 'Copiar o conte&uacute;do deste rascunho para a p&aacute;gina original (e deletar o rascunho)';
+    $t['recreate_original'] = 'Recriar Original';
+    $t['no_drafts_found'] = 'Nenhum Rascunho Encontrado';
+    $t['original_page'] = 'P&aacute;gina Original';
+    $t['template'] = 'Modelo';
+    $t['modified'] = 'Modificado'; // Date of last modification
+    $t['preview'] = 'Visualiza&ccedil;&atilde;o';
+    $t['confirm_delete_draft'] = 'Voc&ecirc; tem certeza de que quer deletar estes rascunhos?';
+    $t['confirm_delete_selected_drafts'] = 'Voc&ecirc; tem certeza de que quer deletar os rascunhos selecionados?';
+    $t['confirm_apply_selected_drafts'] = 'Voc&ecirc; tem certeza de que quer aplicar os rascunhos selecionados?';
+    $t['view_all_drafts'] = 'Visualizar todos os rascunhos';
+    $t['original_deleted'] = 'ORIGINAL DELETADO'; // Upper case
+
+    // Addendum to Version 1.2 /////////////////////////////////////
+    // Nested Pages
+    $t['parent_page'] = 'P&aacute;gina Principal';
+    $t['page_weight_desc'] ='Quanto maior o valor, menos a pasta aparecer&aacute; na lista. Pode ser colocado negativo';
+    $t['active'] = 'Ativo';
+    $t['inactive'] = 'Inativo';
+    $t['menu'] = 'Menu';
+    $t['menu_text'] = 'Texto do Menu';
+    $t['show_in_menu'] = 'Mostrar no menu';
+    $t['not_shown_in_menu'] = 'N&atilde;o mostrar no menu';
+    $t['leave_empty'] = 'Deixar vazio para usar o t&iacute;tulo da p&aacute;gina';
+    $t['menu_link'] = 'Link do Menu';
+    $t['link_url'] ='Esta p&aacute;gina aponta para o seguinte local';
+    $t['link_url_desc'] = 'Pode ser deixado em branco';
+    $t['separate_window'] = 'Abrir em janela separada';
+    $t['pointer_page'] = 'Indicador de P&aacute;gina';
+    $t['points_to_another_page'] = 'Aponta para outra p&aacute;gina';
+    $t['points_to'] = 'Aponta para';
+    $t['redirects'] = 'Redirecionamentos';
+    $t['masquerades'] = 'M&aacute;scaras';
+    $t['strict_matching'] = 'Marcar como selecionada no menu para todas as p&aacute;ginas, a seguir, neste link';
+    $t['up'] = 'Mover para cima';
+    $t['down'] = 'Mover para baixo';
+    $t['remove_template_completely'] = 'Deletar todas as p&aacute;ginas e rascunhos deste modelo para remov&ecirc;-lo completamente';
+    $t['remove_uncloned_template_completely'] = 'Deletar todos os rascunhos deste modelo para remov&ecirc;-lo completamente';
+
+    // Addendum to Version 1.2.5 /////////////////////////////////////
+    // Gallery
+    $t['bulk_upload'] = 'Upload';
+    $t['folder_empty'] = 'Pasta vazia. Por favor use o bot&atilde;o de upload a baixo para adicionar imagens.';
+    $t['root'] = 'Fonte';
+    $t['item'] = 'imagem'; // Single
+    $t['items'] = 'imagens'; // Multiple
+    $t['container'] = 'pasta'; // Single
+    $t['containers'] = 'pastas'; // Multiple
+
+    //
+    $t['columns_missing'] = 'Algumas colunas est&atilde;o desaparecidas!';
+    $t['confirm_delete_columns'] = 'Voc&ecirc; tem certeza de que quer deletar as colunas desaparecidas permanentemente?';
+    $t['add_row'] = 'Adicionar uma linha';
